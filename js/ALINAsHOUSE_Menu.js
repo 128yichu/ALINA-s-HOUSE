d26d2d3d95ad964cdb694d08f2c70c20b81b0c05
DESKTOP-9VIPES5\20171025 <128yichu128@gmail.com>
2020-12-02 21:16:50 +0800

    firstCommit

---
+++ js/ALINAsHOUSE_Menu.js
@@ -0,0 +1,9 @@
+// $('.appetizer .big:nth(0)').click(function(){
+
+//     $('.photo:nth(1)').css('background','url(../images/Menu/soup.jpg)');
+
+// });
+
+$('.appetizer .big:nth(0)').click(function(){
+    $('.photo:nth(1)').attr('src','./images/Menu/soup1.jpg');
+});
